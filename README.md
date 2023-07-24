a39d0806d5912c313de939b3f43f3dc3e200fb95
Elman Laguna <laguna.elman@heb.com>
2023-07-24 14:13:59 +0000

    Adding TwoSum

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # codeforge
 Where serpents slither through oxidized labyrinths, weaving tales of codes untold. Traverse, if you dare, the enigmatic dance of dual languages, forging paths unknown
+
+# Rust links
+- [Rust By Example](https://doc.rust-lang.org/rust-by-example/)
+- [Rust Book](https://rust-book.cs.brown.edu/)\
+- [How to learn Rust](https://www.youtube.com/watch?v=2hXNd6x9sZs)
+- [Rustlings](https://github.com/rust-lang/rustlings)
+
--- python/src/code_forge/two_sum.py
+++ python/src/code_forge/two_sum.py
@@ -1,4 +1,21 @@
 from typing import List
 
+
 def two_sum(nums: List[int], target: int) -> List[int]:
-    return True
+    """Given an array of integers nums and an integer target, 
+       return indices of the two numbers such that they add up to target.
+
+       You may assume that each input would have exactly one solution, 
+       and you may not use the same element twice.
+
+       You can return the answer in any order.
+
+        Args:
+            nums (List[int]): _description_
+            target (int): _description_
+
+        Returns:
+            List[int]: _description_
+    """
+
+    
