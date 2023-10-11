2fef536726017d2d53f5d1b9c683617fec4dc5ad
Elman Steve Laguna <32938596+salsasteve@users.noreply.github.com>
2023-10-10 21:28:45 -0500

    Time: 89 ms (40.61%), Space: 15.2 MB (7.9%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -5,37 +5,27 @@ class Solution(object):
         :rtype: int
         """
 
-        water = 0
+        # Initial total trapped water
+        trapped_water = 0
         
-        l = 0
-        r = len(height) - 1
+        # Pointers initialized at the leftmost and rightmost bars
+        left_pointer, right_pointer = 0, len(height) - 1
 
-        max_left = height[0]
-        max_right = height[len(height) - 1]
+        # Variables to keep track of the tallest bar from the left and right up to the current position
+        max_left, max_right = height[left_pointer], height[right_pointer]
 
-        
-
-        while l < r :
+        # Iterate till the two pointers meet
+        while left_pointer < right_pointer:
             
-            if height[l] < height[r]:
-                l += 1
-                max_left = max(height[l], max_left)
-                water += max_left - height[l]
+            # If the bar at the current left pointer is shorter than the bar at the current right pointer
+            if height[left_pointer] < height[right_pointer]:
+                left_pointer += 1
+                max_left = max(height[left_pointer], max_left)  # Update max from the left
+                trapped_water += max_left - height[left_pointer]  # Calculate trapped water at the current position
             else:
-                r -= 1
-                max_right = max(height[r], max_right)
-                water += max_right - height[r]
+                # Do the same logic for the right pointer if the bar at the right pointer is shorter
+                right_pointer -= 1
+                max_right = max(height[right_pointer], max_right)
+                trapped_water += max_right - height[right_pointer]
 
-
-        return water
-
-
-
-    
-
-
-        
-
-
-
-        
+        return trapped_water
